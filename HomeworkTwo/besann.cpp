11395bf1beebd45aa9b5615221802cfb67866726
besan-issa <sci_223601@omu.edu.ly>
2024-08-12 04:52:09 +0200

    Add files via upload

---
+++ HomeworkTwo/besann.cpp
@@ -0,0 +1,12 @@
+
+#include<iostream>
+using namespace std ;
+int main (){
+string name = "besan issa hamad ";
+int id = 223601 ;
+cout<<"my name is "<<name<<"and my ID is "<<id<<endl;
+
+
+
+    return 0;
+}
