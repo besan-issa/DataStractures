df2d8438e20465aa87875e2ac5fc09eb46cdef24
besan-issa <sci_223601@omu.edu.ly>
2024-08-13 19:40:50 +0200

    Add files via upload

---
+++ HomeworkThree/Factorial.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std ;
+int factorial(int n){
+
+if (n==1)
+return 1;
+return n*factorial(n-1);
+
+}
+int main (){
+int number;
+int fact ;
+cout<<"enter a number : "<<endl; 
+cin>>number;
+cout<<"the factorial is "<<factorial(number);
+return 0;
+}
