b398cb3a229ddc2f7e7aa1f42f5f578781c0412b
besan-issa <sci_223601@omu.edu.ly>
2024-08-31 04:23:37 +0200

    Add files via upload

--- HomeworkFour/StudentPointer.cpp
+++ HomeworkFour/StudentPointer.cpp
@@ -7,11 +7,16 @@ struct student {
     };
 int main (){
     student*p1 = new student {"John Doe " , 1001 , 20};
-    cout<<"Student 1 : "<<endl<<"Name : "<<p1->Name<<endl<<"ID : "<<p1->ID<<endl<<"Age : "<<p1->Age<<endl;
+    cout<<"Student 1 : "<<endl<<"Name : "<<p1->Name<<endl<<"ID : "<<p1->ID<<endl<<"Age : "<<p1->Age<<endl<<endl;
     student*p2 = new student {"Jane smith " , 1002 , 21};
-    cout<<"Student 2 : "<<endl<<"Name : "<<p2->Name<<endl<<"ID : "<<p2->ID<<endl<<"Age : "<<p2->Age<<endl;
+    cout<<"Student 2 : "<<endl<<"Name : "<<p2->Name<<endl<<"ID : "<<p2->ID<<endl<<"Age : "<<p2->Age<<endl<<endl;
     student*p3 = new student {"Bob johnson " , 1003 , 19};
-    cout<<"Student 3 : "<<endl<<"Name : "<<p3->Name<<endl<<"ID : "<<p3->ID<<endl<<"Age : "<<p3->Age<<endl;
+    cout<<"Student 3 : "<<endl<<"Name : "<<p3->Name<<endl<<"ID : "<<p3->ID<<endl<<"Age : "<<p3->Age<<endl<<endl;
+
+    //Free the dynamically allocated memory
+    delete p1;
+    delete p2;
+    delete p3;
     
     
     return 0;
